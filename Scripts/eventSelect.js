1c06fa767b3f41ecdac5eebb0bc5f3462287abe3
Renfred Harper <Renfred93@gmail.com>
2013-05-22 16:46:11 -0400

    Disable event select options on default

--- Scripts/eventSelect.js
+++ Scripts/eventSelect.js
@@ -1,12 +1,35 @@
-var eventOptions = {location:'none',attendance:'none',type:'none',addOns:[]};
+$(document).ready(function() {
 
-$('.dropSelect').click(function(event) {
-	//var content = $(this).text();
-	//$(this).text(content+' \u2714')
-	num = $(this).text();
-	console.log(num);
-	$('.eventImg').attr("src",'Images/'+num+'.jpg');
-	//prevents the dropdown from closing after click
-	event.stopPropagation();	
+	var location = 'none';
+	var attendance = 'none';
+	var type = 'none';
+	var addOns = [];
+	//Other options cannot be selected unless a location is specified
+	$("#eventType-btn, #attendance-btn, #necessities-btn").addClass("disabled");
+	
+	$(".location-menu .dropSelect").click(function(event) {
+		location = $(this).text();
+		//Enable event type option after a location is selected
+		$("#eventType-btn").removeClass("disabled");
+	});
+	$("#eventType-menu .dropSelect").click(function(event) {
+		type = $(this).text();
+		//Enable event type option after a location is selected
+		$("#attendance-btn").removeClass("disabled");	
+	});
+	$("#attendance-menu .dropSelect").click(function(event){
+		num = $(this).text();
+		$('.eventImg').attr("src",'Images/'+num+'.jpg'); //for testing
+		$("#necessities-btn").removeClass("disabled");
+	});
+	$("#necessities-menu .dropSelect").click(function(event){
+		addOns.push($(this).text());
+	});
 });
 
+//Notes
+//prevents the dropdown from closing after click
+//event.stopPropagation();
+//Check Mark	
+//var content = $(this).text();
+//$(this).text(content+' \u2714')
