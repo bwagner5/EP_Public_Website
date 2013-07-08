b5234b970b55be54150019b52e171b0e83ff221f
Renfred Harper <Renfred93@gmail.com>
2013-07-08 14:43:34 -0400

    Footer format changes
    Added link to staff page
    Updated format of footer.php script

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,26 @@
-<?php $root = '/newSite/';
-print('
+<?php 
+	$root = '/newSite/'; 
+	if( $_SERVER['REQUEST_URI'] == $root.'index.shtml' || $_SERVER['REQUEST_URI'] == $root) {
+		$home = true; 
+	}
+	else {
+		$home = false;
+	}
+?>
 <div class="footer">
-    <p>Copyright © '); echo date('Y'); print(' George Mason University</p>
+    <p>Copyright © <?php echo date('Y'); ?> George Mason University</p>
     <div class="socialBar">
-        <a href="http://twitter.com/epgmu"><img src="'); if( $_SERVER['REQUEST_URI'] !== $root.'index.shtml' && $_SERVER['REQUEST_URI'] !== $root){ print('../');}print('images/fc-webicon-twitter.png"></a>
-        <a href="http://instagram.com/gmueventsproduction"><img src="'); if( $_SERVER['REQUEST_URI'] !== $root.'index.shtml' && $_SERVER['REQUEST_URI'] !== $root){ print('../');} print('images/fc-webicon-instagram.png"></a>
-        <a href="http://www.facebook.com/EventsProductionGmu"><img src="'); if( $_SERVER['REQUEST_URI'] !== $root.'index.shtml' && $_SERVER['REQUEST_URI'] !== $root){ print('../');} print('images/fc-webicon-facebook.png"></a>
+        <a href="http://twitter.com/epgmu"><img src="<?php if(!$home){ print('../');} ?>images/fc-webicon-twitter.png"></a>
+        <a href="http://instagram.com/gmueventsproduction"><img src="<?php if(!$home){ print('../');} ?>images/fc-webicon-instagram.png"></a>
+        <a href="http://www.facebook.com/EventsProductionGmu"><img src="<?php if(!$home){ print('../');} ?>images/fc-webicon-facebook.png"></a>
     </div>
     <p><em>Official website of Events Production, an office of <a href="http://studentcenters.gmu.edu/">Student Centers</a> at <a href="http://www.gmu.edu/">George Mason University</em></a>
     </p>
-</div>');
+    <?php if($home) { 
+    print ('
+	    <a href="staff" id="staffLink">
+	    	Enter Staff Site
+	    </a>
+    '); 
+    }
 ?>
