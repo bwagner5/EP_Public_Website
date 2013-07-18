3dfe468d8a27796c73aac5ab219bf1d8026fa2dd
Renfred Harper <Renfred93@gmail.com>
2013-07-18 11:22:02 -0400

    Add main html tables for staff contacs

--- staff/staffContacts.php
+++ staff/staffContacts.php
@@ -31,7 +31,51 @@
 				</div>
 	 		</a>
 			<div class="row-fluid">
+				<div class="span12">
+					<h2 class="pageTitle">Staff Contacts</h2>
+				</div>
+			</div>
+			<div class="spacer"></div>
+			<div class="row-fluid">
+				<div class="span10 offset1">
+					<div class="sectionTitle">EP Staff Contacts</div>
+					<table id="staffContacts">
+						<tr>
+							<td>Ken Locke</td>
+							<td>klocke</td>
+							<td>Office - 703 993 3554<br>Cell - 703 993 3554</td>
+						</tr>
+						<tr>
+							<td>Sean Cox</td>
+							<td>scox8</td>
+							<td>Office - 703 993 3554<br>Cell - 703 993 3554</td>
+						</tr>
+						<tr>
+							<td>Office Phone</td>
+							<td>ep</td>
+							<td>703 993 3773</td>
+						</tr>
+						<tr>
+							<td>Bat Phone</td>
+							<td></td>
+							<td>703 993 2881</td>
+						</tr>
+					</table>
+					<div class="sectionTitle">EP Technician Contacts</div>
+					<table id="staffContacts">
+						<tr>
+							<td>Renfred Harper</td>
+							<td>rharper2</td>
+							<td>703 362 3554</td>
+						</tr>
+					</table>
+				</div>
 		  </div>
+		  <div class="row-fluid">
+				<div class="span5 offset1">
+					<a href="../staff">Back to staff homepage >></a>
+				</div>
+			</div>
 		</div>
 	  </div>
 	  <?php include "../includes/footer.php"; ?>
