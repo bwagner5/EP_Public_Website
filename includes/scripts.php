25b8896ec43c44716f8d6f5a87a0f7d684355117
Renfred Harper <Renfred93@gmail.com>
2013-08-30 19:59:43 -0400

    Add script.php

---
+++ includes/scripts.php
@@ -0,0 +1,13 @@
+<?php 
+	$root = '/~renfred/EP_Public_Website/'; 
+	if( $_SERVER['REQUEST_URI'] == $root.'index.shtml' || $_SERVER['REQUEST_URI'] == $root) {
+		$home = true; 
+	}
+	else {
+		$home = false;
+	}
+?>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="../scripts/bootstrap.min.js"></script>
+	<script src="<?php if(!$home){ print('../');} ?>scripts/document.js"></script>
+	
