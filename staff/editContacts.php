7beae042a446b8406fbeeec14fc4fb2ae9e1ec27
Renfred Harper <Renfred93@gmail.com>
2013-12-11 17:15:17 -0500

    Replace default JSON encoding functions with JSON.php
    The production server’s version of php does not have access to the json encoding library, so an external library has been included instead

--- staff/editContacts.php
+++ staff/editContacts.php
@@ -9,11 +9,23 @@
 	    header("Location: index.php");
 		die(); 
 	}
-
+	// JSON.php includes functions necessary for encoding and decoding json files.
+	// Normally one would use the built-in php json_encode and json_decode functions
+	// but they aren't available in the production server's make of PHP. Thus, JSON.php
+	// is the Pear library's equivilent functions for handling json. 
+	include_once("JSON.php");
+	include_once("file_utilities.php");
+	
+	// Create a new object which contains the static json methods. 
+	// Note SERVICES_JSON_LOOSE_TYPE option is required to retrun decoded
+	// json as arrays instead of objects.
+	$jsonLib = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
+    
     if(isset($_POST['submit'])){
         $file = file_get_contents('files/contacts/contactData.json');
-        $data = json_decode(stripslashes($file), true);
-        
+        $data = $jsonLib->decode(stripslashes($file), true);
+
+        print_r($data);
         if ($_POST['submit'] == "Add"){
             $name = trim($_POST['contactName']);
             $email = trim($_POST['contactEmail']);
@@ -28,10 +40,10 @@
             unset($data[$name]); 
         }
         // Delete the contact directory and re-add it to prevent permissions conflicts
-        rmdir("files/contacts");
+        rrmdir("files/contacts");
         mkdir("files/contacts", $mode = 0775);
         // Save the file
-        file_put_contents('files/contacts/contactData.json', json_encode($data));
+        file_put_contents('files/contacts/contactData.json', $jsonLib->encode($data));
         unset($data); // Release memory
         header('Location: index.php');
     }
